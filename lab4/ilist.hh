4bc73bbdf15873c5457dc1948e37b1b702bd2a77
226472 <dawcio95.95@gmail.com>
2017-03-30 17:18:28 +0200

    Add files via upload

---
+++ lab4/ilist.hh
@@ -0,0 +1,12 @@
+
+
+class ilist
+{
+	
+virtual int size(); //zwraca rozmiar listy
+virtual void add(int, int); //dodaje element na wskazane miejsce
+virtual void remove(int); //usuwa element ze wskazanego miejsca
+virtual int get (int) //zwraca element listy 
+virtual int find(int) //zwraca pozycję elementu listy
+
+}
---
+++ lab4/iqueue.hh
@@ -0,0 +1,7 @@
+
+class iqueue
+{
+virtual int size(); // zwraca rozmiar kolejki
+virtual void add(); //dodaje element na koniec kolejki
+virtual void remove(); //usuwa element z początku kolejki
+}
