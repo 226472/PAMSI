9bdf9e3bc832caf8539f00361bcd79e9c2104f9e
226472 <dawcio95.95@gmail.com>
2017-03-30 17:13:44 +0200

    Add files via upload

---
+++ lab3/istack.hh
@@ -0,0 +1,8 @@
+
+
+class istack
+{
+virtual int size(); //zwraca rozmiar stosu
+virtual void add(int); // dodaje element na stos(zawsze na górę)
+virtual void remove(); // usuwa element ze stosu(zawsze z góry)
+}
