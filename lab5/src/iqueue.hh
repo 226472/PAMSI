7f50906b7ae4ac4c5312e43391a41b3ef23c79ba
226472 <dawcio95.95@gmail.com>
2017-05-31 01:07:33 +0200

    Delete iqueue.hh

--- lab5/src/iqueue.hh
+++
@@ -1,8 +0,0 @@
-
-class iqueue
-{
-virtual int size()=0; // zwraca rozmiar kolejki
-virtual void enqueue(int)=0; //dodaje element na koniec kolejki
-virtual int dequeue()=0; //usuwa element z początku kolejki
-virtual int front()=0;
-};
